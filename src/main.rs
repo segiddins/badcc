c28a7d61459cd9c2d307713ce01bd8fbdb071cc4
Samuel Giddins <segiddins@segiddins.me>
2025-08-07 12:31:11 +0800

    driver improvements

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@
 
 use miette::{Context, IntoDiagnostic, Result, bail};
 use std::{
-    fs::{self, File, read_to_string},
+    fs::{File, read_to_string},
     io::Write,
-    ops::Deref,
     path::{Path, PathBuf},
     process::Command,
 };
@@ -24,29 +23,6 @@ mod lexer;
 mod parser;
 mod tacky;
 
-struct OwnedPath(PathBuf, bool);
-impl Drop for OwnedPath {
-    fn drop(&mut self) {
-        if !self.1 {
-            let _ = fs::remove_file(&self.0);
-        }
-    }
-}
-
-impl Deref for OwnedPath {
-    type Target = PathBuf;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<Path> for OwnedPath {
-    fn as_ref(&self) -> &Path {
-        self
-    }
-}
-
 #[derive(clap::Parser)]
 struct Driver {
     input: PathBuf,
@@ -63,13 +39,17 @@ struct Driver {
 
     #[clap(long, hide = true)]
     keep_artifacts: bool,
+
+    #[clap(skip)]
+    artifacts: Vec<PathBuf>,
 }
 
 impl Driver {
-    fn run(self) -> Result<()> {
+    fn run(mut self) -> Result<()> {
         let pre = self.preprocess()?;
+        self.artifacts.push(pre.clone());
 
-        let src = read_to_string(pre.as_ref())
+        let src = read_to_string(&pre)
             .into_diagnostic()
             .with_context(|| format!("failed to read {}", pre.display()))?;
 
@@ -91,13 +71,14 @@ impl Driver {
         }
 
         let assembly = self.emit_asm(&program)?;
+        self.artifacts.push(assembly.clone());
 
         self.assemble(assembly).context("Assembling failed")?;
 
         Ok(())
     }
 
-    fn preprocess(&self) -> Result<OwnedPath> {
+    fn preprocess(&self) -> Result<PathBuf> {
         let path = self.input.with_extension("i");
         let result = Command::new("gcc")
             .arg("-E")
@@ -109,21 +90,21 @@ impl Driver {
             .and_then(|mut c| c.wait())
             .into_diagnostic()?;
         if result.success() {
-            Ok(OwnedPath(path, self.keep_artifacts))
+            Ok(path)
         } else {
             bail!("preprocessing {} failed", path.display())
         }
     }
 
-    fn emit_asm(&self, program: &Program) -> Result<OwnedPath> {
+    fn emit_asm(&self, program: &Program) -> Result<PathBuf> {
         let path = self.input.with_extension("s");
         let mut f = File::create(&path).into_diagnostic()?;
         emit_asm(program, &f).into_diagnostic()?;
         f.flush().into_diagnostic()?;
-        Ok(OwnedPath(path, self.keep_artifacts))
+        Ok(path)
     }
 
-    fn assemble(&self, assembly: OwnedPath) -> Result<()> {
+    fn assemble(&self, assembly: impl AsRef<Path>) -> Result<()> {
         #[cfg(target_os = "macos")]
         let mut cmd = {
             let mut c = Command::new("arch");
@@ -133,7 +114,7 @@ impl Driver {
         #[cfg(not(target_os = "macos"))]
         let mut cmd = Command::new("gcc");
 
-        cmd.arg(assembly.as_path()).arg("-o").arg(
+        cmd.arg(assembly.as_ref()).arg("-o").arg(
             self.output
                 .clone()
                 .unwrap_or_else(|| self.input.with_extension("")),
@@ -145,6 +126,16 @@ impl Driver {
             Ok(())
         } else {
             bail!("{cmd:?}")
+        }
+    }
+}
+
+impl Drop for Driver {
+    fn drop(&mut self) {
+        if !self.keep_artifacts {
+            for artifact in self.artifacts.drain(..) {
+                let _ = std::fs::remove_file(artifact);
+            }
         }
     }
 }
